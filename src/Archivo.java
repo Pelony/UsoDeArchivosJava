9273793d28246a828fda1baed334a35bceed0fd4
Pelony <andrade.jonathane11@gmail.com>
2022-09-10 19:53:38 -0500

    Segundo commit
    En este commit se editan varias clases para el funcionamiento del programa

--- src/Archivo.java
+++ src/Archivo.java
@@ -5,53 +5,58 @@ public class Archivo {
     Scanner sc=new Scanner(System.in);
     int noControl,Semestre;
     float promedio;
-    String nombre;
+    String nombre,opc="";
     public void Altas(){
-        System.out.println("Altas");
-        System.out.println("Ingresa el Numero de control del alumno");
-        noControl=sc.nextInt();
-        System.out.println("Ingresa el Nombre del alumno");
-        nombre=sc.next();
-        System.out.println("Ingresa el semestre del alumno");
-        Semestre=sc.nextInt();
-        System.out.println("Ingresa el promedio del alumno");
-        promedio=sc.nextFloat();
-        Alumno AltaAlumno= new Alumno(nombre,noControl,Semestre,promedio);
-        FileOutputStream fichero1=null;
-        FileOutputStream fichero2=null;
-        if(promedio<=6.9){
-            try {
-                fichero1 = new FileOutputStream("1.txt");
-                ObjectOutputStream pipe = new ObjectOutputStream(fichero1);
-                pipe.writeObject(AltaAlumno);
-            }catch (FileNotFoundException e){
-                e.printStackTrace();
-            }catch (IOException ex){
-                ex.printStackTrace();
-            }finally {
-                try{
-                    fichero1.close();
+        do{
+            System.out.println("Altas");
+            System.out.println("Ingresa el Numero de control del alumno");
+            noControl=sc.nextInt();
+            System.out.println("Ingresa el Nombre del alumno");
+            nombre=sc.next();
+            System.out.println("Ingresa el semestre del alumno");
+            Semestre=sc.nextInt();
+            System.out.println("Ingresa el promedio del alumno");
+            promedio=sc.nextFloat();
+            Alumno AltaAlumno= new Alumno(nombre,noControl,Semestre,promedio);
+            FileOutputStream fichero1=null;
+            FileOutputStream fichero2=null;
+            if(promedio<=69){
+                try {
+                    fichero1 = new FileOutputStream("1.txt");
+                    ObjectOutputStream pipe = new ObjectOutputStream(fichero1);
+                    pipe.writeObject(AltaAlumno);
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
                 }catch (IOException ex){
                     ex.printStackTrace();
+                }finally {
+                    try{
+                        fichero1.close();
+                    }catch (IOException ex){
+                        ex.printStackTrace();
+                    }
                 }
-            }
-        }else{
-            try {
-                fichero2 = new FileOutputStream("2.txt");
-                ObjectOutputStream pipe = new ObjectOutputStream(fichero2);
-                pipe.writeObject(AltaAlumno);
-            }catch (FileNotFoundException e){
-                e.printStackTrace();
-            }catch (IOException ex){
-                ex.printStackTrace();
-            }finally {
-                try{
-                    fichero2.close();
+            }else{
+                try {
+                    fichero2 = new FileOutputStream("2.txt");
+                    ObjectOutputStream pipe = new ObjectOutputStream(fichero2);
+                    pipe.writeObject(AltaAlumno);
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
                 }catch (IOException ex){
                     ex.printStackTrace();
+                }finally {
+                    try{
+                        fichero2.close();
+                    }catch (IOException ex){
+                        ex.printStackTrace();
+                    }
                 }
             }
-        }
+            System.out.println("Si ya no quieres ingresar mas alumnos teclea si");
+            opc=sc.next();
+        }while(opc.contains("s"));
+
     }
     public void Modificar(){
         System.out.println("Modificar");
